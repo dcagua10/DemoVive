4aaeb6dd22c5b1f39b33b37297911031bc154bb7
Daniel Cagua <d.cagua10@uniandes.edu.co>
2019-03-01 18:49:34 -0500

    commit inicial

---
+++ Assets/Cagua/CustomScripts/AxeCutter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Rigidbody))]
+public class AxeCutter : MonoBehaviour
+{
+
+    public Material capMaterial;
+
+    // Use this for initialization
+
+    private void OnCollisionEnter(UnityEngine.Collision collision)
+    {
+
+        GameObject victim = collision.collider.gameObject;
+        GameObject[] pieces = BLINDED_AM_ME.MeshCut.Cut(victim, transform.position, transform.right, capMaterial);
+
+        if (!pieces[1].GetComponent<Rigidbody>())
+            pieces[1].AddComponent<Rigidbody>();
+
+        Destroy(pieces[1], 1);
+    }
+
+}
